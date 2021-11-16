c331cb6bdc6a7586141ecf0e6ba8ab1d9e25a6af
derslayr10 <derslayr10@gmail.com>
2021-11-16 05:15:47 -0600

    Add project files.

---
+++ GreenhouseConstruction/Custom_Buildings/Greenhouse/Custom_Greenhouse_Building.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using PyTK.CustomElementHandler;
+using StardewValley;
+using StardewValley.Buildings;
+
+namespace GreenhouseConstruction.Custom_Buildings.Greenhouse
+{
+    public class Custom_Greenhouse_Building : Building, ISaveElement
+    {
+
+        private static readonly BluePrint BluePrint = new BluePrint("SpecialGreenhouse");
+
+        public Custom_Greenhouse_Building() : base(Custom_Greenhouse_Building.BluePrint, Vector2.Zero) {
+        
+
+        
+        }
+
+        protected override GameLocation getIndoors(string nameOfIndoorsWithoutUnique)
+        {
+            return new Custom_Greenhouse_Location();
+        }
+
+        public Dictionary<string, string> getAdditionalSaveData()
+        {
+            return new Dictionary<string, string>();
+        }
+
+        public object getReplacement()
+        {
+
+            Mill building = new Mill(new BluePrint("SpecialGreenhouse"), new Vector2(this.tileX.Value, this.tileY.Value));
+            building.indoors.Value = this.indoors.Value;
+            building.daysOfConstructionLeft.Value = this.daysOfConstructionLeft.Value;
+            building.tileX.Value = this.tileX.Value;
+            building.tileY.Value = this.tileY.Value;
+            return building;
+
+        }
+
+        public void rebuild(Dictionary<string, string> additionalSaveData, object replacement)
+        {
+
+            Mill building = (Mill)replacement;
+
+            this.indoors.Value = building.indoors.Value;
+            this.daysOfConstructionLeft.Value = building.daysOfConstructionLeft.Value;
+            this.tileX.Value = building.tileX.Value;
+            this.tileY.Value = building.tileY.Value;
+
+            this.indoors.Value.map = Game1.content.Load<xTile.Map>("Maps\\GreenhouseInterior");
+            this.indoors.Value.updateWarps();
+            this.updateInteriorWarps();
+
+        }
+    }
+}
---
+++ GreenhouseConstruction/Custom_Buildings/Greenhouse/Custom_Greenhouse_Location.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PyTK.CustomElementHandler;
+using StardewValley;
+using StardewValley.Buildings;
+
+namespace GreenhouseConstruction.Custom_Buildings.Greenhouse
+{
+    public class Custom_Greenhouse_Location : GameLocation, ISaveElement
+    {
+
+        public Custom_Greenhouse_Location() : base("Maps\\Greenhouse", "SpecialGreenhouse") {
+
+            this.IsGreenhouse = true;
+        
+        }
+
+        public Dictionary<string, string> getAdditionalSaveData()
+        {
+            var data = new Dictionary<string, string>();
+            if (this.uniqueName.Value != null) {
+
+                data.Add("greenhouse_construction", this.uniqueName.Value);
+            
+            }
+
+            return data;
+
+        }
+
+        public object getReplacement()
+        {
+
+            Shed custom_Greenhouse = new Shed("Maps\\Greenhouse", "SpecialGreenhouse");
+            foreach (Vector2 key in this.objects.Keys) {
+
+                custom_Greenhouse.objects.Add(key, this.objects[key]);
+            
+            }
+
+            foreach (Vector2 key in this.terrainFeatures.Keys)
+            {
+
+                custom_Greenhouse.terrainFeatures.Add(key, this.terrainFeatures[key]);
+
+            }
+
+            return custom_Greenhouse;
+
+        }
+
+        public void rebuild(Dictionary<string, string> additionalSaveData, object replacement)
+        {
+
+            Shed custom_Greenhouse = (Shed)replacement;
+
+            if (additionalSaveData.TryGetValue("greenhouse_construction", out string savedName)) {
+
+                this.uniqueName.Value = savedName;
+            
+            }
+
+            foreach (Vector2 key in custom_Greenhouse.objects.Keys) {
+
+                this.objects.Add(key, custom_Greenhouse.objects[key]);
+            
+            }
+
+            foreach (Vector2 key in custom_Greenhouse.terrainFeatures.Keys) {
+
+                this.terrainFeatures.Add(key, custom_Greenhouse.terrainFeatures[key]);
+            
+            }
+
+        }
+    }
+}
